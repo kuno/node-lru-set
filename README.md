08ad9115d8ba6df3f86a00874fc7206df4fb7625
Guan Qing <kuno.the.developer@gmail.com>
2013-07-15 14:29:18 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# lru-set [![Build Status](https://secure.travis-ci.org/kuno/lru-set.png?branch=master)](http://travis-ci.org/kuno/lru-set)
+
+The best project ever.
+
+## Getting Started
+Install the module with: `npm install lru-set`
+
+```javascript
+var lru_set = require('lru-set');
+lru_set.awesome(); // "awesome"
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Guan Qing  
+Licensed under the MIT license.
---
+++ test/lru-set.mocha.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var should = require('should');
+
+var LRUSET = require('../lib/lru-set');
+
+describe('LRUSET', function() {
+    before(function(done) {
+        process.nextTick(done);
+    });
+    after(function(done) {
+        process.nextTick(done);
+    });
+
+    describe('Class', function() {
+        it('should be a function', function() {
+            LRUSET.should.be.a('function');
+        });
+    });
+
+    describe('Instance', function() {
+        var set = LRUSET(1, 2, 3);
+
+        it('should has methods', function() {
+            set.add.should.be.a('function');
+            set.pop.should.be.a('function');
+            set.len.should.be.a('function');
+            set.contain.should.be.a('function');
+        });
+
+        it('should not add duplicated item', function() {
+            set.add(3);
+            set.len().should.equal(3);
+        });
+
+        it('should add unique item', function() {
+            set.add(4);
+            set.len().should.equal(4);
+        });
+
+        it('should tell if an item is inside', function() {
+            set.add(5);
+            set.contain(5).should.equal(true);
+            set.contain(6).should.equal(false);
+        });
+    });
+});
